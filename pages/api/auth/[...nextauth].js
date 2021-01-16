99faf17b5d9943965eba35f9eb519bff735b402f
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-01-16 00:18:20 -0800

    auth: implement google

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,9 +4,30 @@ import Providers from 'next-auth/providers';
 const options = {
   // Configure one or more authentication providers
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    Providers.Email({
+      // SMTP connection string or nodemailer configuration object https://nodemailer.com/
+      server: process.env.NEXTAUTH_EMAIL_SERVER,
+      // Email services often only allow sending email from a valid/verified address
+      from: process.env.NEXTAUTH_EMAIL_FROM,
+    }),
+    // When configuring oAuth providers make sure you enabling requesting
+    // permission to get the users email address (required to sign in)
+    // Providers.Apple({
+    //   clientId: process.env.NEXTAUTH_APPLE_ID,
+    //   clientSecret: process.env.NEXTAUTH_APPLE_KEY_SECRET,
+    //   clientSecretCallback: false,
+    // }),
+    Providers.Google({
+      clientId: process.env.NEXTAUTH_GOOGLE_ID,
+      clientSecret: process.env.NEXTAUTH_GOOGLE_SECRET,
+    }),
+    Providers.Facebook({
+      clientId: process.env.NEXTAUTH_FACEBOOK_ID,
+      clientSecret: process.env.NEXTAUTH_FACEBOOK_SECRET,
+    }),
+    Providers.Twitter({
+      clientId: process.env.NEXTAUTH_TWITTER_ID,
+      clientSecret: process.env.NEXTAUTH_TWITTER_SECRET,
     }),
     // ...add more providers here
   ],
