a3ca91657c046dfff30ee10027945ae37f4edccb
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-01-28 22:26:18 -0800

    chore: store entire past code inside "monorepo-legacy" folder + wip: yarn workspace & lerna

--- monorepo-legacy/packages/app/new-app/src/pages/index.tsx
+++ monorepo-legacy/packages/app/new-app/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { NextPage } from 'next'
 import { Button } from 'components/button'
-import { capitalize } from '@yourcompany/functions'
+import { capitalize } from 'packages/shared/functions'
 
 const Index: NextPage = () => {
   const handleClick = (): void => {
